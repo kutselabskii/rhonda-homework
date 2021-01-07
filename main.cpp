953885e40bb34ea651e6c30223c1c2a33d3c0cce
Egor Kutselabskii <osutanfirst@gmail.com>
2021-01-07 14:55:40 +1000

    Change input/output system, print time measurements to console

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <chrono>
+#include <fstream>
 
 int main() {
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-
 	auto start = std::chrono::high_resolution_clock::now();
 
 	int n;
-	std::cin >> n;
+	std::ifstream input;
+	input.open("input.txt");
+	input >> n;
+	input.close();
 
 	bool* numbers = new bool[n + 1];
 	for (int i = 0; i <= n; ++i) {
@@ -24,23 +25,26 @@ int main() {
 		}
 	}
 
+	std::ofstream output;
+	output.open("output.txt");
 	bool multiple = false;
 	for (int i = 2; i <= n; ++i) {
 		if (numbers[i]) {
 			if (multiple) {
-				std::cout << ", ";
+				output << ", ";
 			}
-			std::cout << i;
+			output << i;
 			multiple = true;
 		}
 	}
+	output.close();
 
 	delete[] numbers;
 
 	auto stop = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 
-	std::cout << std::endl << "Elapsed time: " << duration.count() << " ms";
+	std::cout << "Elapsed time: " << duration.count() << " ms";
 
 	return 0;
 }
